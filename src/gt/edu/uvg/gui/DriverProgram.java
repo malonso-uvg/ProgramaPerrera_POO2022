23029ccceeed285ed0588440937cbce7dc446d75
Moises Alonso <moises.alonso@telus.com>
2022-08-01 09:16:04 -0600

    Carga inicial del proyecto Perrera 2022

---
+++ src/gt/edu/uvg/gui/DriverProgram.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gt.edu.uvg.gui;
+
+/**
+ * @author moises.alonso
+ *
+ */
+public class DriverProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Este serel el programa de Administrador de Perreras 2022");
+	}
+
+}
