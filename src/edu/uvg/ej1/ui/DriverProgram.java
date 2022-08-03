6ef2d96b82de7a213bddba14eb12ce2122c840f8
Gxrco <gersonrc111@icloud.com>
2022-08-03 09:10:48 -0600

    Grupo 6

--- src/edu/uvg/ej1/ui/DriverProgram.java
+++ src/edu/uvg/ej1/ui/DriverProgram.java
@@ -1,4 +1,4 @@
-/**
+;/**
  * 
  */
 package edu.uvg.ej1.ui;
@@ -8,14 +8,29 @@ package edu.uvg.ej1.ui;
  *
  */
 public class DriverProgram {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		System.out.println("Prueba");
 	}
 
+	public void ShowDogData(Dog perro) {
+		System.out.println("Printing dog Data B)\nPress SPACE to show data");
+		Scanner SAOPAPU = new Scanner(System.in);
+		SAOPAPU.nextLine();
+		System.out.println("The dog name is:" + perro.getName());
+		System.out.println("The breed is:" + perro.getBreed(););
+		System.out.println("The weight is:" + perro.getWeight(););
+		System.out.println("The dog primary color is:" + perro.getPrimaryColor());
+		System.out.println("The dog second color is:" + perro.getSecondColor());
+	}
+
+	public void ShowFriendData(Friend amigo) {
+		System.out.println("Printing Friend Data B)\nPress SPACE to show data");
+		Scanner TACOGHOULPAPU = new Scanner(System.in);
+		TACOGHOULPAPU.nextLine();
+		System.out.println("The friend actual money name is:" + amigo.getActualMoney());
+		System.out.println("The friend's dog is lost: " + amigo.getDogIsLost(););
+	}
 }
